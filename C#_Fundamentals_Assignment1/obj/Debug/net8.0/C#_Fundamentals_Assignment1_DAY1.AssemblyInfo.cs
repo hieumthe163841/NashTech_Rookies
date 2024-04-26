124f331dbf6f6c000ba9ebb24ca1377d6dc3e8b8
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-04-26 08:09:24 +0700

    update file .gitgnore

--- C#_Fundamentals_Assignment1/obj/Debug/net8.0/C#_Fundamentals_Assignment1_DAY1.AssemblyInfo.cs
+++ C#_Fundamentals_Assignment1/obj/Debug/net8.0/C#_Fundamentals_Assignment1_DAY1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Fundamentals_Assignment1_DAY1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01fefff66811a03db9b01074a9801ca3a6d5d8d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Fundamentals_Assignment1_DAY1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Fundamentals_Assignment1_DAY1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
